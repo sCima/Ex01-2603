66ef7026f7ccef0b0b66cb862021ca7403149b36
Rodolfera <sanches.cima@gmail.com>
2024-03-26 11:40:35 -0300

    Uso de mapas

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         Usuario lucas = new Usuario(01, "Lucas", "123");
-        Usuario fepile = new Usuario(02, "Fepile", "123");
+        Usuario fepile = new Usuario(03, "Fepile", "123");
 
+        System.out.println(lucas.compareTo(fepile));
+        System.out.println(fepile.compareTo(lucas));
         System.out.println(fepile.compareTo(fepile));
+
+        Map mapa = new HashMap();
+
+        mapa.put("Lucas", lucas);
+
+        Object objeto = mapa.get("Lucas");
+
+        Usuario usuario = (Usuario) objeto;
+        System.out.println(usuario.getNome());
     }
 
 }
